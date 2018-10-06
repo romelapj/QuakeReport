a6d34cde188fa8fbcae4903dfc3751c93c1bfbae
Julio Cesar Garcia Sabogal <julio.garcia@rappi.com>
2018-10-06 11:07:13 -0500

    update pull

---
+++ app/src/main/java/com/romelapj/quakereport/EarthquakeActivity.kt
@@ -0,0 +1,12 @@
+package com.romelapj.quakereport
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EarthquakeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_earthquake)
+    }
+}
